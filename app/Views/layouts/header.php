a89cdbc7578c968e77249ee1a2d10e2d06e79fe0
smnde <des.mn69@gmail.com>
2022-08-25 13:09:39 +0700

    some changes

--- app/Views/layouts/header.php
+++ app/Views/layouts/header.php
@@ -5,16 +5,15 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<!-- Bootstrap & Jquery ui css -->
-	<link rel="stylesheet" href="<?= base_url('bootstrap/dist/css/bootstrap.min.css'); ?>">
-	<link rel="stylesheet" href="<?= base_url('jqueryui/jquery-ui.css') ?>">
-	<link rel="stylesheet" href="<?= base_url('jqueryui/jquery-ui.theme.css'); ?>">
-	<link rel="stylesheet" href="<?= base_url('jqueryui/jquery-ui.structure.css'); ?>">
+	<link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.css'); ?>">
+	<link rel="stylesheet" href="<?= base_url('assets/css/jquery-ui.css'); ?>">
+	<link rel="stylesheet" href="<?= base_url('assets/css/jquery-ui.theme.css'); ?>">
+	<link rel="stylesheet" href="<?= base_url('assets/css/jquery-ui.structure.css'); ?>">
 
-	<!-- Bootstrap, Jquery & jquery ui js -->
-	<script src="<?= base_url('bootstrap/dist/js/bootstrap.min.js'); ?>"></script>
-	<script src="<?= base_url('jqueryui/external/jquery/jquery.js'); ?>"></script>
-	<script src="<?= base_url('jqueryui/jquery-ui.min.js'); ?>"></script>
-	<script src=""></script>
+	<!-- Bootstrap & Jquery -->
+	<script src="<?= base_url('assets/js/bootstrap.bundle.js'); ?>"></script>
+	<script src="<?= base_url('assets/js/jquery.js'); ?>"></script>
+	<script src="<?= base_url('assets/js/jquery-ui.js'); ?>"></script>
 	<title><?= $title; ?></title>
 </head>
 <body>
